32d9f8c1b4f22173c1bc5e25fb0c244db7ef9d4e
mubashir-73 <mubashirsheriff7@gmail.com>
2024-06-10 20:45:46 +0530

    Implement Parser to Split ByteStream

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,3 +1,7 @@
+#include <cassert>
+#include <cerrno>
+#include <cstddef>
+#include <cstdint>
 #include <cstring>
 #include <iostream>
 #include <netdb.h>
@@ -7,6 +11,71 @@
 
 #define MYPORT "3420"
 #define Backlog 10
+
+// Implementing parser here:
+
+static int32_t read_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = recv(fd, buf, n, 0);
+    if (rv <= 0) {
+      return -1;
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+static int32_t write_full(int fd, char *buf, size_t n) {
+  while (n > 0) {
+    ssize_t rv = send(fd, buf, n, 0);
+    if (rv <= 0) {
+      return -1;
+    }
+    assert((size_t)rv <= n);
+    n -= (size_t)rv;
+    buf += rv;
+  }
+  return 0;
+}
+
+const size_t k_max_msg = 4096;
+static int32_t one_request(int new_fd) {
+  char rbuf[4 + k_max_msg + 1];
+  errno = 0;
+  int32_t err = read_full(new_fd, rbuf, 4);
+  if (err) {
+    if (errno == 0) {
+      std::cout << "EOF reached~";
+    } else {
+      std::cout << "read() Error";
+    }
+
+    return err;
+  }
+  u_int32_t len = 0;
+  memcpy(&len, rbuf, 4);
+  if (len > k_max_msg) {
+    std::cout << "Message too long";
+    return -1;
+  }
+  err = read_full(new_fd, &rbuf[4], len);
+  if (err) {
+    std::cout << "read() error\n";
+    return err;
+  }
+  rbuf[4 + len] = '\0';
+  std::cout << "Client says: " << &rbuf[4] << "\n";
+  const char Reply[] = "world";
+  char wbuf[4 + sizeof(Reply)];
+  len = (uint32_t)strlen(Reply);
+  memcpy(wbuf, &len, 4);
+  memcpy(&wbuf[4], Reply, len);
+  return write_full(new_fd, wbuf, 4 + len);
+}
+
+// Response here:
 
 int main() {
   struct sockaddr_storage their_addr;
@@ -34,15 +103,12 @@ int main() {
       std::cout << "error accepting connection restarting loop...\n";
       continue;
     }
-    std::cout << "connection accepted...\n";
-    std::string buffer;
-    char temp_buffer[256];
-    int len = 255;
-    std::cout << "Recieving message....\n";
-    recv(new_fd, temp_buffer, len, 0);
-    std::cout << "Message Recieved!\n";
-    buffer = std::string(temp_buffer);
-    std::cout << "Message: " << buffer << std::endl;
+    while (true) {
+      int32_t err = one_request(new_fd);
+      if (err) {
+        break;
+      }
+    }
     int close(new_fd);
   }
   return 0;
